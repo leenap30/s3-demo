1706e5745805756bd8f4a618c0809358b94dc965
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-05-23 20:26:20 +0530

    Create hostweb.tf

---
+++ hostweb.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "b" {
+  bucket = "s3-website-test.hashicorp.com"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+}
